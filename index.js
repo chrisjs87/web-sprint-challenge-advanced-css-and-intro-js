b661e8e59536b3ad036d618a93b64efb24853138
Christopher Scrantom <christopher.scrantom@gmail.com>
2021-04-16 11:37:40 -0500

    Task 4 complete

--- index.js
+++ index.js
@@ -231,7 +231,7 @@ console.log(artists[8].name);
  Example, if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
 function getArtistByIndex(array, index) {
-  return `the artist at index ${index} is ${array[index].name}`
+  return `the artist at index ${index} is ${array[index].name}`;
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -242,11 +242,15 @@ Use get20s to do the following:
 Example born in 1901 and died in 1959 - included -- born in 1889 and died in 1925 not included
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/*Your Code Here*/){
-  /*Your Code Here*/
+function get20s(array){
+  let newArray = [];
+  for(let i = 0; i < array.length; i++){
+    if(array[i].years.substring(0,4) >= 1900 && array[i].years.substring(7,11) < 2000){    //it wanted to include 1900 but exclude 2000
+      newArray.push(array[i].name);
+    }
+  }
+  return newArray;
 }
-
-
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
  Use removeArtist to do the following:
